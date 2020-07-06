53ec782d1ad514d7b9e81e634af1119e3b7dd49b
freeke84 <67484521+freeke84@users.noreply.github.com>
2020-07-06 22:52:25 +0200

    Update Configuration.h

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -338,7 +338,7 @@
  * Enable Start & End G-code in SETTINGS -> FEATURE menu.
  */
 // Start G-code - run this G-code before starting print
-#define PRINT_START_GCODE "G28 XY R10\n" // Raise Z 10mm before homing X & Y
+#define PRINT_START_GCODE "G28 XY R10\n G29" // Raise Z 10mm before homing X & Y
 
 // End G-code - run this G-code after finishing print
 #define PRINT_END_GCODE "G90\nG1 E-4\nG92 E0\nM18\n" // Switch to absolute positioning, reduce filament pressure by performing small retract, reset extruder position, disable steppers
